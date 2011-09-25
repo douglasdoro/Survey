59b425e6cd1f55cb3849f20d9cb20cf53172b1b8
Douglas Dorô <douglasdoro@gmail.com>
2011-09-25 14:59:26 -0300

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MiniAppSurvey::Application.config.secret_token = '6b6e19461a55deb2532fae5279a8b016d681ab1ed64d5b49f567df72ba7bdebac03a39fefbf8946fe36409d59e6ddeb035242d1e8e7c9dc2632abb79c6da0c31'
