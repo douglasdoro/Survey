74450bd933d2da48fd5435b78fccc22b91264944
Douglas Dorô <douglasdoro@gmail.com>
2011-09-27 16:27:53 -0300

    creted answer's model

---
+++ app/models/answer.rb
@@ -0,0 +1,7 @@
+class Answer < ActiveRecord::Base
+
+  belongs_to :question
+  
+  validates :content,  :presence => true
+
+end
--- app/models/question.rb
+++ app/models/question.rb
@@ -1,5 +1,9 @@
 class Question < ActiveRecord::Base
+
   belongs_to :survey
+  has_many   :answers, :dependent => :destroy
+  
+  accepts_nested_attributes_for :answers, :allow_destroy => true 
   
   validates :content, :presence => true 
 end
